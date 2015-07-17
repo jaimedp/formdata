d3a162b4e466a7cfb368ba3c85c0f35668f891ab
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-07-17 10:56:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+$('#upload-file').on('submit', function (e) {
+    e.preventDefault();
+    var data, xhr;
+    data = new FormData();
+    var inputControl = $('#file')[0];
+    data.append('file', inputControl.files[0]);
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        processData: false,
+        data: data,
+        headers: {
+            'authorization': '',
+            'content-type': 'multipart/form-data'
+        }
+
+    });
+});
+
